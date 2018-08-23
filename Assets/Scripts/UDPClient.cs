fb3184c146b928bbd6b5c6b83f88073ebfd722d7
Diego de Rochebouet <ddegrimaudetderocheb@itba.edu.ar>
2018-08-23 12:53:01 -0300

    Unity project starting files with simple UDP client server

---
+++ Assets/Scripts/UDPClient.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using UnityEngine;
+
+public class UDPClient : MonoBehaviour
+{
+	public string Ip_raies;
+	public int Port_raies;
+    private UdpClient udpClient;
+
+	void Start () {
+	    udpClient = new UdpClient();
+        udpClient.Connect(Ip_raies, Port_raies);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.S))
+		{
+			Byte[] sendBytes = Encoding.ASCII.GetBytes("Is anybody there?");
+	        udpClient.Send(sendBytes, sendBytes.Length);
+			Debug.Log("Sended!");
+		}
+
+	}
+	private void OnDisable()
+	{
+		udpClient.Close();
+	}
+	
+	private void OnApplicationQuit()
+	{
+//		udpClient.Close();
+	}
+}
---
+++ Assets/Scripts/UDPServer.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using UnityEngine;
+
+public class UDPServer : MonoBehaviour
+{
+
+	public int Port_client;
+	private Queue<String> messages;
+	private UdpClient udpClient;
+	private IPEndPoint RemoteIpEndPoint;
+	private Thread theUDPServer;
+
+	private byte[] byteArray;
+	// Use this for initialization
+	void Start()
+	{
+		udpClient = new UdpClient(Port_client);
+		RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, Port_client);
+		theUDPServer = new Thread(new ThreadStart(serverThread));
+		theUDPServer.Start();
+	}
+
+	private void OnDisable()
+	{
+		theUDPServer.Abort();
+		udpClient.Close();
+	}
+
+//	private void OnApplicationQuit()
+//	{
+//		theUDPServer.Abort();	
+//		udpClient.Close();
+//	}
+
+
+	public void serverThread()
+	{
+		try
+		{
+			int i = 0;
+			while (true)
+			{
+				i += 1;
+				Byte[] receiveBytes = udpClient.Receive(ref RemoteIpEndPoint);
+//				int receiveBytes = udpClient.Client.ReceiveFrom(byteArray, ref RemoteIpEndPoint);
+				string returnData = Encoding.ASCII.GetString(receiveBytes);
+				lock (messages)
+				{
+					messages.Enqueue(returnData);
+				}
+			}
+		}
+		catch (Exception e)
+		{
+			print(e);
+		}
+		
+	}
+
+	private void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.R))
+		{
+			lock (messages)
+			{
+				Debug.Log(messages.Dequeue());
+			}
+		}
+	}
+}
