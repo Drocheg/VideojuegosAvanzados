fd4fde8ffca10d6a4064b32d3ac67ddbf9b95e09
Diego de Rochebouet <ddegrimaudetderocheb@itba.edu.ar>
2018-08-30 19:02:20 -0300

    testing bitWriter

--- Assets/Scripts/UDPClient.cs
+++ Assets/Scripts/UDPClient.cs
@@ -11,29 +11,44 @@ public class UDPClient : MonoBehaviour
 {
 	public string Ip_server;
 	public int Port_server;
-    private UdpClient udpClient;
-    private MemoryStream ms;
-    private BitWriter bitWriter;
+	private UdpClient udpClient;
+	private MemoryStream ms;
 
-	void Start () {
-	    udpClient = new UdpClient();
-        udpClient.Connect(Ip_server, Port_server);
-        ms = new MemoryStream();
-        bitWriter = new BitWriter(ms);
-    }
+	private BitWriter bitWriter;
+	private BitReader bitReader;
 	
+	//private BitWriter bitReader;
+
+	void Start()
+	{
+		udpClient = new UdpClient();
+		udpClient.Connect(Ip_server, Port_server);
+		ms = new MemoryStream();
+		bitWriter = new BitWriter(ms);		
+		bitReader = new BitReader(ms);
+	}
+
 	// Update is called once per frame
-	void Update () {
+	void Update()
+	{
 		if (Input.GetKeyDown(KeyCode.S))
 		{
-            bitWriter.WriteBit(true);
-            bitWriter.Flush();
-            Debug.Log(ms.Length);
-	        udpClient.Send(ms.GetBuffer(), (int) ms.Length);
-			Debug.Log("Sended!");
+			for (var i = 0; i < 32; i++)
+			{
+				bitWriter.WriteBit(i % 2 == 0);
+			}
+			//bitWriter.Flush();
+			Debug.Log(ms.Length);
+			// udpClient.Send(ms.GetBuffer(), (int) ms.Length);
+			Debug.Log("Sent!");
 		}
 
+		if (Input.GetKeyDown(KeyCode.R))
+		{
+			Debug.Log(bitReader.ReadBit());
+		}
 	}
+	
 	private void OnDisable()
 	{
 		udpClient.Close();
