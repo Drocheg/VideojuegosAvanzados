0daffde251631b6d57f1054bd631403d347f31e7
Tomas Agustin Raies <traies@itba.edu.ar>
2018-10-18 16:36:40 -0300

    Character animations

---
+++ Assets/PlayerInputController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInputController : MonoBehaviour {
+	private Animator _playerAnimator;
+	// Use this for initialization
+	void Start () {
+		_playerAnimator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var strafe = Input.GetAxis("Horizontal");
+		var speed = Input.GetAxis("Vertical");
+
+		_playerAnimator.SetFloat("Speed", speed);
+		_playerAnimator.SetFloat("Strafe", strafe);
+	}
+
+}
