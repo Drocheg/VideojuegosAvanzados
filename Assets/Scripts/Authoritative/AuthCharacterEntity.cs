9e31a1d80a581958be314032b613badfbb41c649
Tomas Agustin Raies <traies@itba.edu.ar>
2018-11-08 12:03:22 -0300

    fix rounding error

--- Assets/Scripts/Authoritative/AuthCharacterEntity.cs
+++ Assets/Scripts/Authoritative/AuthCharacterEntity.cs
@@ -41,7 +41,7 @@ public class AuthCharacterEntity : CharacterEntity, IAuth {
 		writer.WriteFloat(_animator.GetFloat("Run"), -1, 1, _world.AnimationStep);
 		writer.WriteFloat(transform.eulerAngles.y, 0, 360, _world.RotationStep);
 		writer.WriteInt(lastProcessedInput, 0, (uint) _world.MaxMoves);
-		writer.WriteFloat(Mathf.Max(_healthManager._hp, 0), 0, _world.MaxHP, 1);
+		writer.WriteFloat(Mathf.Max(_healthManager._hp, 0), 0, _world.MaxHP, 0.1f);
 	}
 
 	public void Move(MoveCommand command) {
