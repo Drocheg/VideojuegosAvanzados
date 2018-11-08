015498dee67f9ad15df990364d992967215e5509
Tomas Agustin Raies <traies@itba.edu.ar>
2018-11-08 11:31:00 -0300

    Auth side movement

--- Assets/Scripts/Authoritative/AuthPlayer.cs
+++ Assets/Scripts/Authoritative/AuthPlayer.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class AuthPlayer : MonoBehaviour {
-
+	PlayerManager _playerManager;
+	AuthWorld _authWorld;
+	public float Speed;
+	CharacterController _characterController;
 	// Use this for initialization
 	void Start () {
-		
+		_playerManager = GetComponent<PlayerManager>();
+		_authWorld = GameObject.FindObjectOfType<AuthWorld>();
+		_characterController = GetComponent<CharacterController>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		_characterController.Move(_playerManager.Run * Speed * Time.deltaTime * transform.TransformDirection(Vector3.forward));
+		_characterController.Move(_playerManager.Strafe * Speed * Time.deltaTime * transform.TransformDirection(Vector3.right));
 	}
 }
