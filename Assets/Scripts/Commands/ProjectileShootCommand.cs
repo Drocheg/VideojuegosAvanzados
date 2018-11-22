ec69a9f18b039a4ed084904ba3fd24e8bd51a19f
Tomas Agustin Raies <traies@itba.edu.ar>
2018-11-22 13:01:59 -0300

    added shooterid to projectile command

--- Assets/Scripts/Commands/ProjectileShootCommand.cs
+++ Assets/Scripts/Commands/ProjectileShootCommand.cs
@@ -4,9 +4,9 @@ public class ProjectileShootCommand {
 	public float _x, _y, _z;
 	public float _minX, _minY, _minZ, _maxX, _maxY, _maxZ, _positionPrecision;
 	public float _dirX, _dirY, _dirZ;
-	public int _id, _maxId;
+	public int _id, _maxId, _shooterId;
 
-	public ProjectileShootCommand(int id, int maxId, float x, float y, float z, float dirX, float dirY, float dirZ, float minX, float maxX, float minY, float maxY, float minZ, float maxZ, float positionPrecision) {
+	public ProjectileShootCommand(int id, int maxId, int shooterId, float x, float y, float z, float dirX, float dirY, float dirZ, float minX, float maxX, float minY, float maxY, float minZ, float maxZ, float positionPrecision) {
 		_x = x;
 		_y = y;
 		_z = z; 
@@ -21,6 +21,7 @@ public class ProjectileShootCommand {
 		_maxZ = maxZ;
 		_positionPrecision = positionPrecision;
 		_id = id;
+		_shooterId = shooterId;
 		_maxId = maxId;
 	}
 
@@ -28,6 +29,7 @@ public class ProjectileShootCommand {
 		// Serialize Command number
 		writer.WriteInt((uint) NetworkCommand.PROJECTILE_SHOOT_COMMAND, 0, (uint) System.Enum.GetValues(typeof(NetworkCommand)).Length);
 		writer.WriteInt((uint) _id, 0, (uint) _maxId);
+		writer.WriteInt((uint) _shooterId, 0, (uint) _maxId);
 		writer.WriteFloat(_x, _minX, _maxX, _positionPrecision);
 		writer.WriteFloat(_y, _minY, _maxY, _positionPrecision);
 		writer.WriteFloat(_z, _minZ, _maxZ, _positionPrecision);
@@ -38,12 +40,13 @@ public class ProjectileShootCommand {
 
 	public static ProjectileShootCommand Deserialize(BitReader reader, int maxId, float minX, float maxX, float minY, float maxY, float minZ, float maxZ, float positionPrecision) {
 		var id = reader.ReadInt(0, maxId);
+		var shooterId = reader.ReadInt(0, maxId);
 		var x = reader.ReadFloat(minX, maxX, positionPrecision);
 		var y = reader.ReadFloat(minY, maxY, positionPrecision);
 		var z = reader.ReadFloat(minZ, maxZ, positionPrecision);
 		var dirX = reader.ReadFloat(minX, maxX, positionPrecision);
 		var dirY = reader.ReadFloat(minY, maxY, positionPrecision);
 		var dirZ = reader.ReadFloat(minZ, maxZ, positionPrecision);
-		return new ProjectileShootCommand(id, maxId, x, y, z, dirX, dirY, dirZ, minX, maxX, minY, maxY, minZ, maxZ, positionPrecision);
+		return new ProjectileShootCommand(id, maxId, shooterId, x, y, z, dirX, dirY, dirZ, minX, maxX, minY, maxY, minZ, maxZ, positionPrecision);
 	}
 }
